76081feec1c3eaf7c29d661a9dda777c104dc9e1
Alexandre Conte <alexandreconte.dev@gmail.com>
2024-04-19 22:14:19 -0300

    ui: cria header

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <>
+      <header className="
+        fixed z-10
+        flex-col-center text-center
+        w-screen h-[48px] p-2.5
+        primary-color
+        text-lg
+        text-zinc-50
+      ">
+        <h1>Busca Números Primos</h1>
+      </header>
+      <div className="h-[48px]" />
+    </>
+  )
+}
