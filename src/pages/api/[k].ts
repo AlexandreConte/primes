642df058e5c4df876339850b121b3608ee8d9f71
Alexandre Conte <alexandreconte.dev@gmail.com>
2024-04-22 19:30:13 -0300

    API: adiciona retorno de tempo de execucao em ms

--- src/pages/api/[k].ts
+++ src/pages/api/[k].ts
@@ -1,10 +1,10 @@
 import primeNumbersLessThan from "@/utils/primeNumberLessThan";
 
 export default function handle(req: any, res: any) {
+  const start = performance.now();
   const { k }: { k: number } = req.query;
-  const obj = {
-    k,
-    primeNumbersLessThanK: primeNumbersLessThan(k)
-  };
-  res.status(200).send(obj);
+  const primeNumbersLessThanK = primeNumbersLessThan(k);
+  const end = performance.now();
+  const timer = end - start;
+  res.status(200).send({ k, primeNumbersLessThanK, timer });
 }
